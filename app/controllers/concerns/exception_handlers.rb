114f4b8b1eefeb6cc977937284873116e4354ccf
Paa Yaw <paayaw.dev@gmail.com>
2022-06-01 05:58:44 +0000

    Add more error classes in ExceptionHandlers module

--- app/controllers/concerns/exception_handlers.rb
+++ app/controllers/concerns/exception_handlers.rb
@@ -4,15 +4,29 @@ module ExceptionHandlers
   class AuthenticationError < StandardError; end 
   class InvalidTokenError < StandardError; end 
   class MissingTokenError < StandardError; end
+  class ExpiredTokenError < StandardError; end
 
   included do 
     rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ExceptionHandlers::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandlers::ExpiredTokenError, with: :unauthorized_request
+    rescue_from ExceptionHandlers::InvalidTokenError, with: :invalid_record
+    rescue_from ExceptionHandlers::MissingTokenError, with: :invalid_record
   end
 
   
   private 
 
+  def unauthorized_request(e)
+    render json: { message: e.message }, status: 401
+  end
+
+  def record_not_found(e)
+    render json: { message: e.message }, status: 404
+  end
+
   def invalid_record(e)
-    render json: { message: e.message }, status: :unprocessable_entity
+    render json: { message: e.message }, status: 422
   end
 end
